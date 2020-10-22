6c62a6b4bfd22ae39d63b372e150c822e467c411
fullname <you@example.com>
2020-10-22 23:33:25 +0300

    make it better

--- blogger/blogger/settings.py
+++ blogger/blogger/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
- 
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", __file__)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
